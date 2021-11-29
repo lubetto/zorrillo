dc3b171064b26e3dda26f4dc8f00c4628b3376d5
lubetto <lubetto@gmail.com>
2021-11-29 10:47:30 -0500

    Corrección Buenas prácticas UserController.java

--- backend/catalogo/src/main/java/zorrillo/catalogo/controller/UserController.java
+++ backend/catalogo/src/main/java/zorrillo/catalogo/controller/UserController.java
@@ -7,30 +7,44 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+/**
+ *
+ * @author LUIS GERMAN ORTEGA M.
+ */
 @RestController
 @RequestMapping("api/user")
 @CrossOrigin("*")
 public class UserController {
+    /**
+     * Inyectar Dependencias
+     */
     @Autowired
     private UserService userService;
-
+    /**
+     * Consulta todos los Registros
+     */
     @GetMapping("/all")
     public List<User> getAll() {
         return userService.getAll();
     }
-
+    /**
+     * Crear Nuevo Registro
+     */
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public User registrar(@RequestBody User user) {
         return userService.registrar(user);
     }
-
+    /**
+     * Consulta por Email y password
+     */
     @GetMapping("/{email}/{password}")
     public User autenticarUsuario(@PathVariable("email") String email, @PathVariable("password") String password) {
         return userService.autenticarUsuario(email, password);
     }
-
+    /**
+     * Consulta por Email
+     */
     @GetMapping("/{email}")
     public boolean existeEmail(@PathVariable("email") String email) {
         return userService.existeEmail(email);
